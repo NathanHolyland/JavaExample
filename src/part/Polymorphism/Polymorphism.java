1e5a451bb8ee8fb403574928a0b3d3fc6d7d752b
Nathan Holyland <94959991+NathanHolyland@users.noreply.github.com>
2022-04-19 18:33:03 +0100

    Fix

---
+++ src/part/Polymorphism/Polymorphism.java
@@ -0,0 +1,26 @@
+package part.Polymorphism;
+// Overloading
+
+public class Polymorphism {
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    public static int sum(String a, String b) {
+        try {
+            int num_a = Integer.parseInt(a);
+            int num_b = Integer.parseInt(b);
+            return num_a+num_b;
+        }
+        catch(Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5, 3)+"\n");
+        System.out.println(sum("5", "3")+"\n");
+        System.out.println(sum("asdf", "3"));
+    }
+}
+
